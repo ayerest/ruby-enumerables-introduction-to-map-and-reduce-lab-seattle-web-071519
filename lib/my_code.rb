525832640ef19a293d84ef03826035ae394d4381
Iris S <irisiseve@gmail.com>
2019-06-15 17:54:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,9 @@
 # My Code here....
 def map_to_negativize(array)
-  
+  new_array = []
+  counter = 0
+  while counter < array.length
+    new_array << array[counter] * -1
+    counter += 1
+  end
 end
